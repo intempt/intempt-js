c8a2ae909b14b65994f2b72c6805af55180c1bab
beska0013 <gugushvili.beso0013@gmail.com>
2024-09-09 21:09:28 +0400

    remove: iwe-image-block styles

--- src/intemptJs/modules/choices/choices.config.ts
+++ src/intemptJs/modules/choices/choices.config.ts
@@ -5,7 +5,7 @@ export const ChoicesConfig = {
   *[iwe-hide="true"]{ display: none !important;visibility: hidden !important; opacity: 0 !important;user-select: none !important;pointer-events: none !important;}
   .iwe-text-block{display: block; width: auto;height: auto;padding: 0;margin: 0;font-size: 16px;} 
   .iwe-link{ color: #0080ff; text-decoration: underline;cursor: pointer;}
-  .iwe-image-block{width: 200px;height: 200px;}
+  .iwe-image-block{}
   .iwe-container-block{display: block;width: 100%;height: 100px;padding: 0; margin: 0;}
   `
 
