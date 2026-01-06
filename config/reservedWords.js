be09e2a459537466da670046bf01ef7dcf17928b
Besik Gugushvili <gugushvili.beso0013@gmail.com>
2026-01-06 20:53:58 +0400

    add: guard for $ sighn (#148)
    Co-authored-by: Beso <beso@intempt.com>

---
+++ config/reservedWords.js
@@ -0,0 +1,40 @@
+export const reservedWords = [
+  // jQuery
+  '$', 
+  'jQuery',
+  
+  // Underscore/Lodash
+  '_',
+  
+  // Popular frameworks
+  'React',
+  'Vue',
+  'Angular',
+  'angular',
+  'Backbone',
+  'Ember',
+  
+  // Common utility libraries
+  'axios',
+  'moment',
+  'lodash',
+  'underscore',
+  
+  // Analytics libraries
+  'ga',
+  'gtag',
+  'dataLayer',
+  'analytics',
+  'gtm',
+  
+  // Shopify globals (since you use them)
+  'Shopify',
+  
+  // Other common library globals
+  'require',
+  'exports',
+  'module',
+  'define', // AMD
+  'global',
+  'process',
+];
---
+++ config/reservedWordsPatterns.d.ts
@@ -0,0 +1,2 @@
+export declare function getReservedWordPatterns(word: string): RegExp[];
+export declare function shouldExcludeMatch(word: string, match: string, matchIndex: number, bundleCode: string): boolean;
