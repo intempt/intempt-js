5b92775008f2c11f5a8d58f292610827c1ddc636
Beso <beso@intempt.com>
2026-03-18 03:24:28 +0400

    update

--- __tests__/support/index.ts
+++ __tests__/support/index.ts
@@ -9,7 +9,7 @@ EnvConfig.initFromValues({
   VITE_ENV: 'test',
   VITE_CHOICES_API: 'https://choices-api.test.com',
   VITE_WEB_EDITOR_BASE_LINK: 'https://editor.test.com',
-  VITE_OPENER_LINK: 'https://opener.test.com',
+  VITE_OPENER_LINKS: '["https://opener.test.com"]',
   VITE_WEB_EDITOR_STORAGE_KEY: '__test_editor__',
   VITE_LOCATION_API_URL: '',
   DEV: false,
